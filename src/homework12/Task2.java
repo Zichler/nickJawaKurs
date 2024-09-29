e19879d48b883a8b38ef326f05041bc7f3a82874
Zichler <nikcih99@gmail.com>
2024-09-29 21:05:01 +0200

    homework12 add

---
+++ src/homework12/Task2.java
@@ -0,0 +1,18 @@
+package homework12;
+/* Напишите метод, который вычисляет сумму всех четных чисел в массиве.
+ */
+public class Task2 {
+    public static void main(String[] args) {int[] array = {1, 2, 3, 4, 5, 6}; // Пример массива
+        int sum = sumOfEvenNumbers(array);
+        System.out.println("Сумма всех четных чисел в массиве: " + sum);
+    }
+    public static int sumOfEvenNumbers(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+}
