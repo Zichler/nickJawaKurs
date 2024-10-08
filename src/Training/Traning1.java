2a66dcdfa76b2093ff1261ae67896bee874557f3
Zichler <nikcih99@gmail.com>
2024-10-09 00:27:47 +0200

    homework18 add

--- src/Training/Traning1.java
+++ src/Training/Traning1.java
@@ -2,10 +2,18 @@ package Training;
 
 public class Traning1 {
     public static void main(String[] args) {
-        int age = 36;
-        if (age < 76) {
-            System.out.println( age + " < 76 " + " То я молодой ");
+        double height = 1.85;
+        double weight = 90.5;
+        double index = weight / (height * height);
+        System.out.println("Индекс массы тела: " + index);
+        if (index <= 16) {
+            System.out.println("Выраженный дефицит тела. " + "Необходима срочная консультация врача");
+        } else if (index <= 18.5) {
+            System.out.println("Недостаточная (дефицит) масса тела");
+        } else if (index <= 25) {
+            System.out.println("Норма");
+        } else {
+            System.out.println("Ожирение первой степени");
         }
-
     }
 }
---
+++ src/homework18/CalculatorApp.java
@@ -0,0 +1,17 @@
+package homework18;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+
+        int div = Calculator.divide(15, 3);
+        System.out.println("div: " + div);
+
+        double divDouble = Calculator.divide(10.0, 4);
+        System.out.println("divDouble: " + divDouble);
+
+        int sum = Calculator.add(1, 4, 5, 6, 8, 10);
+        System.out.println("sum: " + sum);
+
+
+    }
+}
