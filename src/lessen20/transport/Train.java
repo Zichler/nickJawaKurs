bcb51e83fe01315430ce3155edc677611f68b7f2
Zichler <nikcih99@gmail.com>
2024-10-10 00:28:47 +0200

    lessen20 add

---
+++ src/lessen20/transport/Train.java
@@ -0,0 +1,42 @@
+package lessen20.transport;
+
+public class Train extends Vehicle {
+
+    private int capacity;
+    private  int countPassengers;
+
+    private int countWagons;
+    private final int wagonCapacity;
+
+    public Train(String model, int year, int wagonCapacity, int countWagons) {
+        super(model, year); // Конструктор родителя
+        this.wagonCapacity = wagonCapacity;
+        this.countWagons = countWagons;
+        calculateCapacity();
+    }
+
+    private void calculateCapacity() {
+        this.capacity = countWagons * wagonCapacity;
+    }
+
+    public void setCountWagons(int countWagons) {
+        this.countWagons = countWagons;
+        calculateCapacity();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getCountPassengers() {
+        return countPassengers;
+    }
+
+    public int getCountWagons() {
+        return countWagons;
+    }
+
+    public int getWagonCapacity() {
+        return wagonCapacity;
+    }
+}
